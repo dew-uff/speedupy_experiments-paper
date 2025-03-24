0b907eb7ebee556065640871dd504f90e20741f5
claytonchagas <claytonchagas1@gmail.com>
2025-03-23 21:09:05 -0300

    Cleaning up the experiment folders, refactoring scripts, and creating the script to generate graphs and tables for the analyses.

--- run_experiments10x.sh
+++ run_experiments10x.sh
@@ -30,25 +30,38 @@ SOURCE_DIR="$ROOT_PATH/speedupy"
 
 # Define the list of destination paths / Define a lista de caminhos de destino
 
-DESTINATIONS=("$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp03_quicksort/quicksort.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp02_look_and_say/look_and_say.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp11_gauss_legendre_quadrature/test_gauss_legendre_quadrature.py" "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/__main__.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp08_fft/test_compute_FFT_speedupy.py" "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp04_curves/curves_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp14_pernicious_numbers/test_pernicious_numbers.py" "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp02_cvar/cvar_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp06_belief_propagation/test_belief_propagation_speedupy.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/basic/basic_spheres.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/walking_colloid/walking_colloid.py" "$ROOT_PATH/diversity-with-speedupy/Diversity_sims/vince_sim_speedupy.py" "$ROOT_PATH/Tiny-GSGP-with-speedupy/adapted_for_speedupy/TINY_GSHCGP.py" "$ROOT_PATH/epr-with-speedupy/analyse_speedupy.py" "$ROOT_PATH/qho-with-speedupy/qho2_speedupy.py")
+# DESTINATIONS=("$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp03_quicksort/quicksort.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp02_look_and_say/look_and_say.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp11_gauss_legendre_quadrature/gauss_legendre_quadrature.py" "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/heat_distribution_lu.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp08_fft/fft_speedupy.py" "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp04_curves/curves_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp14_pernicious_numbers/pernicious_numbers.py" "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp02_cvar/cvar_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp06_belief_propagation/belief_propagation_speedupy.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/basic/basic_spheres.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/walking_colloid/walking_colloid.py" "$ROOT_PATH/diversity-with-speedupy/Diversity_sims/vince_sim_speedupy.py" "$ROOT_PATH/Tiny-GSGP-with-speedupy/adapted_for_speedupy/TINY_GSHCGP.py" "$ROOT_PATH/epr-with-speedupy/analyse_speedupy.py" "$ROOT_PATH/qho-with-speedupy/qho2_speedupy.py")
+
+DESTINATIONS=("$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp03_quicksort/quicksort.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp02_look_and_say/look_and_say.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp11_gauss_legendre_quadrature/gauss_legendre_quadrature.py" "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/heat_distribution_lu.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp08_fft/fft_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp14_pernicious_numbers/pernicious_numbers.py" "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp02_cvar/cvar_speedupy.py" "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp06_belief_propagation/belief_propagation_speedupy.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/basic/basic_spheres.py" "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/walking_colloid/walking_colloid.py" "$ROOT_PATH/diversity-with-speedupy/Diversity_sims/vince_sim_speedupy.py" "$ROOT_PATH/Tiny-GSGP-with-speedupy/adapted_for_speedupy/TINY_GSHCGP.py" "$ROOT_PATH/epr-with-speedupy/analyse_speedupy.py")
+
+# DESTINATIONS=("$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/heat_distribution_lu.py")
 
 # Define the list of arguments for each destination path / Define a lista de argumentos para cada caminho de destino
+# ARGUMENTS_0=("0.1" "0.05" "0.01" "0.005" "0.001")
 # ARGUMENTS_0=("1e1")
 ARGUMENTS_0=("1e1" "1e2" "1e3" "1e4" "1e5") # quicksort
 ARGUMENTS_1=("25" "30" "35" "40" "45") # look_and_say
 ARGUMENTS_2=("1000" "2000" "3000" "4000" "5000") # gauss_legendre_quadrature
-ARGUMENTS_3=("0.1" "0.05" "0.01" "0.005" "0.001") # heat
-ARGUMENTS_4=("1000" "2000" "3000" "4000" "5000")
-ARGUMENTS_5=("1576862 -8567453 1648423 542312 512 -20135 1455678 52349" "4341212 -12312419 123123 5423672 107 20135 145678 52349" "-11124 -11124 62412 1412 107501 201635 15678 57849" "43441212 -22523123 6219 5143228 107501 20135 1455678 5234567849" "-111243412 -124122123 62192412 5281412 107501 201422635 123455678 5234567849")
-ARGUMENTS_6=("20000" "25000" "30000" "35000" "39000")
-ARGUMENTS_7=("1e6" "5e6" "10e6" "50e6" "100e6")
-ARGUMENTS_8=("1000" "2000" "3000" "4000" "5000")
-ARGUMENTS_9=("40" "1000" "10000" "100000" "1000000")
-ARGUMENTS_10=("-10" "-20" "-30" "-40" "-50")
-ARGUMENTS_11=("1000000" "2000000" "3000000" "4000000" "5000000")
-ARGUMENTS_12=("1" "3" "5" "7" "9")
-ARGUMENTS_13=("100" "200" "300" "400" "500")
-ARGUMENTS_14=("100" "500" "1000" "5000" "10000")
+ARGUMENTS_3=("0.1" "0.05" "0.01" "0.005" "0.001") # heat_distribution_lu
+ARGUMENTS_4=("1000" "2000" "3000" "4000" "5000") # fft
+# ARGUMENTS_5=("1576862 -8567453 1648423 542312 512 -20135 1455678 52349" "4341212 -12312419 123123 5423672 107 20135 145678 52349" "-11124 -11124 62412 1412 107501 201635 15678 57849" "43441212 -22523123 6219 5143228 107501 20135 1455678 5234567849" "-111243412 -124122123 62192412 5281412 107501 201422635 123455678 5234567849") # curves
+# ARGUMENTS_6=("20000" "25000" "30000" "35000" "39000") # pernicious_numbers
+# ARGUMENTS_7=("1e6" "5e6" "10e6" "50e6" "100e6") # cvar
+# ARGUMENTS_8=("1000" "2000" "3000" "4000" "5000") # belief_propagation
+# ARGUMENTS_9=("40" "1000" "10000" "100000" "1000000") # dnacc_basic_spheres
+# ARGUMENTS_10=("-10" "-20" "-30" "-40" "-50") # dnacc_walking_colloid
+# ARGUMENTS_11=("1000000" "2000000" "3000000" "4000000" "5000000") # vince_sim
+# ARGUMENTS_12=("1" "3" "5" "7" "9") # TINY_GSHCGP
+# ARGUMENTS_13=("100" "200" "300" "400" "500") # analyse_epr
+ARGUMENTS_5=("20000" "25000" "30000" "35000" "39000") # pernicious_numbers
+ARGUMENTS_6=("1e6" "5e6" "10e6" "50e6" "100e6") # cvar
+ARGUMENTS_7=("1000" "2000" "3000" "4000" "5000") # belief_propagation
+ARGUMENTS_8=("40" "1000" "10000" "100000" "1000000") # dnacc_basic_spheres
+ARGUMENTS_9=("-10" "-20" "-30" "-40" "-50") # dnacc_walking_colloid
+ARGUMENTS_10=("1000000" "2000000" "3000000" "4000000" "5000000") # vince_sim
+ARGUMENTS_11=("1" "3" "5" "7" "9") # TINY_GSHCGP
+ARGUMENTS_12=("100" "200" "300" "400" "500") # analyse_epr
+# ARGUMENTS_14=("100" "500" "1000" "5000" "10000") # qho2
 
 # Copy the source directory to each destination directory / Copia o diretório de origem para cada diretório de destino
 for i in "${!DESTINATIONS[@]}"; do

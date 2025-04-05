3cf561c781fcedf76891cb8b928c3e198477718a
claytonchagas <claytonchagas1@gmail.com>
2025-04-05 12:58:03 -0300

    Testing automation of alternate runs.

--- run_experiments_alternating.sh
+++ run_experiments_alternating.sh
@@ -42,41 +42,41 @@ declare -a DESTINATIONS=(
     "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp03_quicksort/quicksort.py"
     "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp02_look_and_say/look_and_say.py"
     "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp11_gauss_legendre_quadrature/gauss_legendre_quadrature.py"
-    "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/heat_distribution_lu.py"
-    "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp08_fft/fft_speedupy.py"
-    "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp14_pernicious_numbers/pernicious_numbers.py"
-    "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp02_cvar/cvar_speedupy.py"
-    "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp06_belief_propagation/belief_propagation_speedupy.py"
-    "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/basic/basic_spheres.py"
-    "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/walking_colloid/walking_colloid.py"
-    "$ROOT_PATH/diversity-with-speedupy/Diversity_sims/vince_sim_speedupy.py"
-    "$ROOT_PATH/Tiny-GSGP-with-speedupy/adapted_for_speedupy/TINY_GSHCGP.py"
-    "$ROOT_PATH/epr-with-speedupy/analyse_speedupy.py"
-    "$ROOT_PATH/qho-with-speedupy/qho2_speedupy.py"
-    "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp04_curves/curves_speedupy.py"
+    # "$ROOT_PATH/speedupy_experiments/01pilots/01pilots_exp04_heat_distribution_lu/heat_distribution_lu.py"
+    # "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp08_fft/fft_speedupy.py"
+    # "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp14_pernicious_numbers/pernicious_numbers.py"
+    # "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp02_cvar/cvar_speedupy.py"
+    # "$ROOT_PATH/speedupy_experiments/04benchproglangs/04benchpl_exp06_belief_propagation/belief_propagation_speedupy.py"
+    # "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/basic/basic_spheres.py"
+    # "$ROOT_PATH/DNACC-with-speedupy/adapted_for_speedupy/examples/walking_colloid/walking_colloid.py"
+    # "$ROOT_PATH/diversity-with-speedupy/Diversity_sims/vince_sim_speedupy.py"
+    # "$ROOT_PATH/Tiny-GSGP-with-speedupy/adapted_for_speedupy/TINY_GSHCGP.py"
+    # "$ROOT_PATH/epr-with-speedupy/analyse_speedupy.py"
+    # "$ROOT_PATH/qho-with-speedupy/qho2_speedupy.py"
+    # "$ROOT_PATH/speedupy_experiments/05msrgithubexps/05msrgithubexps_exp04_curves/curves_speedupy.py"
 )
 
 ARGUMENTS_0=("1e1" "1e2" "1e3" "1e4" "1e5")
 ARGUMENTS_1=("25" "30" "35" "40" "45")
 ARGUMENTS_2=("1000" "2000" "3000" "4000" "5000")
-ARGUMENTS_3=("0.1" "0.05" "0.01" "0.005" "0.001")
-ARGUMENTS_4=("1000" "2000" "3000" "4000" "5000")
-ARGUMENTS_5=("20000" "25000" "30000" "35000" "39000")
-ARGUMENTS_6=("1e6" "5e6" "10e6" "50e6" "100e6")
-ARGUMENTS_7=("1000" "2000" "3000" "4000" "5000")
-ARGUMENTS_8=("40" "1000" "10000" "100000" "1000000")
-ARGUMENTS_9=("-10" "-20" "-30" "-40" "-50")
-ARGUMENTS_10=("1000000" "2000000" "3000000" "4000000" "5000000")
-ARGUMENTS_11=("1" "3" "5" "7" "9")
-ARGUMENTS_12=("100" "200" "300" "400" "500")
-ARGUMENTS_13=("100" "500" "1000" "5000" "6000")
-ARGUMENTS_14=(
-    "-11124 -11124 62412 1412 107501 201635 15678 57849"
-    "1576862 -8567453 1648423 542312 512 -20135 1455678 52349"
-    "4341212 -12312419 123123 5423672 107 20135 145678 52349"
-    "4341241 -1231219 1231423 5423672 10547 20135 145678 52349"
-    "-1112434 -1241223 6212412 5281412 107501 20142265 3455678 5467849"
-)
+# ARGUMENTS_3=("0.1" "0.05" "0.01" "0.005" "0.001")
+# ARGUMENTS_4=("1000" "2000" "3000" "4000" "5000")
+# ARGUMENTS_5=("20000" "25000" "30000" "35000" "39000")
+# ARGUMENTS_6=("1e6" "5e6" "10e6" "50e6" "100e6")
+# ARGUMENTS_7=("1000" "2000" "3000" "4000" "5000")
+# ARGUMENTS_8=("40" "1000" "10000" "100000" "1000000")
+# ARGUMENTS_9=("-10" "-20" "-30" "-40" "-50")
+# ARGUMENTS_10=("1000000" "2000000" "3000000" "4000000" "5000000")
+# ARGUMENTS_11=("1" "3" "5" "7" "9")
+# ARGUMENTS_12=("100" "200" "300" "400" "500")
+# ARGUMENTS_13=("100" "500" "1000" "5000" "6000")
+# ARGUMENTS_14=(
+#     "-11124 -11124 62412 1412 107501 201635 15678 57849"
+#     "1576862 -8567453 1648423 542312 512 -20135 1455678 52349"
+#     "4341212 -12312419 123123 5423672 107 20135 145678 52349"
+#     "4341241 -1231219 1231423 5423672 10547 20135 145678 52349"
+#     "-1112434 -1241223 6212412 5281412 107501 20142265 3455678 5467849"
+# )
 
 # Função para obter argumentos dinamicamente
 get_argument() {
@@ -99,12 +99,16 @@ echo "=== MODE 1: no-cache ===" / "=== MODO 1: sem cache ==="
 for ((run=1; run<=NUM_RUNS; run++)); do
     echo "---- Run $run ----" / "---- Rodada $run ----"
     for arg_index in {0..4}; do
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
         for i in "${!DESTINATIONS[@]}"; do
             dest_dir=$(dirname "${DESTINATIONS[i]}")
             cd "$dest_dir"
             
             python3.12 "speedupy/setup_exp/setup.py" "${DESTINATIONS[i]}"
-            python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode no-cache | \
+            # python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode no-cache | \
+            python3.12 "${DESTINATIONS[i]}" $(get_argument $i $arg_index) --exec-mode no-cache | \
+
                 tail -n1 | cut -d':' -f2 >> "$ROOT_PATH/outputs/$(basename ${DESTINATIONS[i]} .py)_no_cache.txt"
             
             rm -rf "$dest_dir/.speedupy/"
@@ -129,6 +133,8 @@ for ((run=1; run<=NUM_RUNS; run++)); do
         
         # Setup para cada argumento
         for arg_index in {0..4}; do
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
             export SPEEDUPY_CACHE_DIR=".speedupy_arg${arg_index}"
             echo "Running setup for ${DESTINATIONS[i]}, argument $arg_index" /
             # echo "Executando setup para ${DESTINATIONS[i]}, argumento $arg_index"
@@ -138,6 +144,8 @@ for ((run=1; run<=NUM_RUNS; run++)); do
     
     # Execução normal
     for arg_index in {0..4}; do
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
         for i in "${!DESTINATIONS[@]}"; do
             DEST_DIR=$(dirname "${DESTINATIONS[i]}")
             cd "$DEST_DIR"
@@ -154,6 +162,8 @@ for ((run=1; run<=NUM_RUNS; run++)); do
     
     # Limpeza
     for arg_index in {0..4}; do
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
         for i in "${!DESTINATIONS[@]}"; do
             rm -rf "$(dirname "${DESTINATIONS[i]}")/.speedupy_arg${arg_index}"
         done
@@ -167,12 +177,15 @@ echo "=== MODE 3: intra-exec ===" / "=== MODO 3: intra-exec ==="
 for ((run=1; run<=NUM_RUNS; run++)); do
     echo "---- Run $run ----" / "---- Rodada $run ----"
     for arg_index in {0..4}; do
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
         for i in "${!DESTINATIONS[@]}"; do
             dest_dir=$(dirname "${DESTINATIONS[i]}")
             cd "$dest_dir"
             
             python3.12 "speedupy/setup_exp/setup.py" "${DESTINATIONS[i]}"
-            python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode manual | \
+            # python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode manual | \
+            python3.12 "${DESTINATIONS[i]}" $(get_argument $i $arg_index) --exec-mode manual | \
                 tail -n1 | cut -d':' -f2 >> "$ROOT_PATH/outputs/$(basename ${DESTINATIONS[i]} .py)_intra_exec.txt"
             
             rm -rf "$dest_dir/.speedupy/"
@@ -200,7 +213,11 @@ for ((run=1; run<=NUM_RUNS; run++)); do
         cd "$dest_dir"
         
         for arg_index in {0..4}; do
-            python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode manual | \
+            arg=$(get_argument $i $arg_index)
+            echo "Running ${DESTINATIONS[i]} with argument index $arg_index (arg=$arg, Run $run)"
+            # python3.12 "${DESTINATIONS[i]}" ${ARGUMENTS[i][arg_index]} --exec-mode manual | \
+            python3.12 "${DESTINATIONS[i]}" $(get_argument $i $arg_index) --exec-mode manual | \
+
                 tail -n1 | cut -d':' -f2 >> "$ROOT_PATH/outputs/$(basename ${DESTINATIONS[i]} .py)_intra_exp.txt"
         done
     done
